f7b9e3072c0d81daaf948821e0003b85ebff5341
Ian Ellison-Taylor <ianet@microsoft.com>
2024-11-26 15:33:34 -0800

    ...

--- calc_hashes.jl
+++ calc_hashes.jl
@@ -1,5 +1,9 @@
 using Tar, Inflate, SHA
 
+# NB Don't use Windows GUI to create tar.gz, it'll be missing execute on the dll. 
+# Use WSL eg
+#   tar -czvf ../microsoft.ml.onnxruntime.tar.gz .
+
 filename = "microsoft.ml.onnxruntime.tar.gz"
 println("sha256: ", bytes2hex(open(sha256, filename)))
 println("git-tree-sha1: ", Tar.tree_hash(IOBuffer(inflate_gzip(filename))))
