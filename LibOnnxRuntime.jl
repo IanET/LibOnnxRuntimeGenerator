9f8eaab8d33475a58f696209ebf2e07b5ff4542c
IanET <1154506+IanET@users.noreply.github.com>
2025-11-21 20:05:26 -0800

    ...

--- LibOnnxRuntime.jl
+++ LibOnnxRuntime.jl
@@ -4,8 +4,19 @@ using CEnum: CEnum, @cenum
 
 using Pkg.Artifacts
 
-# TODO - Make arch aware
-const OnnxRuntime = joinpath(artifact"OnnxRuntime", "runtimes\\win-x64\\native\\onnxruntime.dll")
+@static if Sys.iswindows()
+    @static if Sys.ARCH == :x86_64
+        const OnnxRuntime = joinpath(artifact"OnnxRuntime", "runtimes\\win-x64\\native\\onnxruntime.dll")
+    else # Sys.ARCH == :aarch64
+        const OnnxRuntime = joinpath(artifact"OnnxRuntime", "runtimes\\win-arm64\\native\\onnxruntime.dll")
+    end
+elseif Sys.islinux
+    @static if Sys.ARCH == :x86_64
+        const OnnxRuntime = joinpath(artifact"OnnxRuntime", "runtimes/linux-x64/native/libonnxruntime.so")
+    else # Sys.ARCH == :aarch64
+        const OnnxRuntime = joinpath(artifact"OnnxRuntime", "runtimes/linux-arm64/native/libonnxruntime.so")
+    end
+end
 
 
 mutable struct OrtStatus end
