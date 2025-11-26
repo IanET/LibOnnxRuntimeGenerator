593153af11fa7bed6a0859f238a02d9da1ff99b9
IanET <1154506+IanET@users.noreply.github.com>
2025-11-26 13:25:51 -0800

    ...

--- generator.app.jl
+++ generator.app.jl
@@ -1,3 +1,4 @@
+using Clang
 using Clang.Generators
 using Clang.LibClang
 
@@ -6,53 +7,7 @@ cd(@__DIR__)
 options = load_options(joinpath(@__DIR__, "generator.toml"))
 args = get_default_args()
 
-# AI Generated code for the OrtApi struct to create Julia wrappers for each function pointer.
-# function gen_api_function(io::IO, name, return_type, arg_types, arg_names)
-#     # Map C types to Julia types
-#     julia_return_type = Generators.translate(return_type)
-#     julia_arg_types = [Generators.translate(t) for t in arg_types]
-
-#     # Create argument list with type annotations
-#     julia_args = ["$(arg_name)::$(arg_type)" for (arg_name, arg_type) in zip(arg_names, julia_arg_types)]
-#     prepend!(julia_args, ["ort::Ptr{OrtApi}"])
-
-#     println(io, "function $(name)(", join(julia_args, ", "), ")")
-#     print(io, "    ccall(Base.getproperty(ort, :$(name)), $(julia_return_type), (")
-#     print(io, join(julia_arg_types, ", "), "), ")
-#     println(io, join(arg_names, ", "), ")") # Don't pass `ort` to ccall
-#     println(io, "end")
-#     println(io)
-# end
-
-# function rewriter(io::IO, node::Generators.StructDefinition, options)
-#     if node.name != "OrtApi"
-#         # If it's not the struct we want to customize, print the default
-#         Generators.pretty_print(io, node, options)
-#         return
-#     end
-
-#     # It is the OrtApi struct, so run our custom logic
-#     # First, generate the default struct definition
-#     Generators.pretty_print(io, node, options)
-
-#     # Then, generate wrappers for each function pointer
-#     for field in node.fields
-#         name = field.name
-#         func_ptr_type = field.type
-#         # ... (rest of the logic is the same as in common_utils.jl)
-#     end
-# end
-
-# function rewrite(expr::Expr)
-#     @info "Expr type: $expr"
-#     # if expr isa Generators.StructDefinition
-#     #     @info "Rewriting struct: $(expr.name)"
-#     # end
-# end
-
 function gen_api_function(io::IO, struct_name::String, function_name::String, return_type::AbstractJuliaType, arg_types::Vector, arg_names::Vector)
-
-    # Convert to Julia type strings
     jrt = Generators.translate(return_type) 
     jats = [Generators.translate(t) for t in arg_types]
     julia_args = ["pstrct::Ptr{$(struct_name)}", arg_names...]
@@ -72,7 +27,6 @@ function rewrite(io::IO, struct_sym::Symbol, fc::CLFieldDecl)
 
     rt = clang_getResultType(pt)
     return_type = CLType(rt) |> tojulia
-    # crt = clang_getCanonicalType(return_type)
 
     na = clang_getNumArgTypes(pt)
     ats = [clang_getArgType(pt, i) for i in 0:na-1]
@@ -90,8 +44,8 @@ end
 
 function rewrite(dag::ExprDAG)
     struct_sym = :OrtApi # Test
-    n = findfirst(n->n.id == struct_sym, ctx.dag.nodes)
-    node = ctx.dag.nodes[n]
+    n = findfirst(n->n.id == struct_sym, dag.nodes)
+    node = dag.nodes[n]
     c = node.cursor
     t = Clang.getCursorType(c)
     fldc = fields(t)
