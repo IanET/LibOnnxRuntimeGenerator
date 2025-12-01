d98d27d3d2988725551aaa77b34de4e21576b89f
IanET <1154506+IanET@users.noreply.github.com>
2025-12-01 11:49:34 -0800

    ...

--- onnxruntimejl.h
+++ onnxruntimejl.h
@@ -1,3 +1,6 @@
 #define _Frees_ptr_opt_
 
+// Assumes building on Win32 where ORT_CHAR_T gets defined as wchar_t. Redefine it back so we can switch at runtime
+#define wchar_t ORT_CHAR_T
+
 #include ".\microsoft.ml.onnxruntime\build\native\include\onnxruntime_c_api.h"
